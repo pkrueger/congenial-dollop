93dafe96cdec2bce74025401137b022273d8549e
Lumine3449 <>
2022-09-23 18:59:25 -0600

    minor changes

--- client/app/Controllers/CommentsSectionController.js
+++ client/app/Controllers/CommentsSectionController.js
@@ -5,11 +5,20 @@ import { commentsSectionService } from "../Services/CommentsSectionService.js";
 
 function drawComments() {
   let template = ''
-  appState
+  appState.commentsSection.forEach((c) => template += c.commentsSectionTemplate)
+
 }
 
 
+// drawTheComments()
+
 
 export class CommentsSectionController {
+  constructor() {
+
+  }
+
+
+
 
 }
